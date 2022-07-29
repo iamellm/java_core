9745102354f125aff4edffa02d14a7b13e4feb3a
Evgeny Lim <lim.evny@gmail.com>
2022-07-30 05:06:09 +0900

    PositiveNegativeZero exercise

---
+++ src/exercises/PositiveNegativeZero.java
@@ -0,0 +1,20 @@
+package exercises;
+
+public class PositiveNegativeZero {
+    public static void main(String[] args) {
+
+        checkNumber(3);
+        checkNumber(-8);
+        checkNumber(0);
+    }
+
+    public static void checkNumber(int number) {
+        if(number > 0) {
+            System.out.println("positive");
+        } else if(number < 0) {
+            System.out.println("negative");
+        } else {
+            System.out.println("zero");
+        }
+    }
+}
