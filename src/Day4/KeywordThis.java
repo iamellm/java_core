f1679a1a0fc0a11417cc6b210f2be783cd6deb09
Evgeny Lim <lim.evny@gmail.com>
2022-07-31 04:07:28 +0900

    Keyword this

---
+++ src/Day4/KeywordThis.java
@@ -0,0 +1,27 @@
+package Day4;
+
+public class KeywordThis {
+    public static void main(String[] args) {
+        Human human = new Human();
+        human.setName("Alina");
+        human.setAge(24);
+        human.getInfo();
+    }
+}
+
+class Human {
+    String name;
+    int age;
+
+    public void setName(String name) {this.name = name;}
+
+    public void setAge(int age) {this.age = age;}
+
+    public String getName() {return name;}
+
+    public int getAge() { return age;}
+
+    public void getInfo() {
+        System.out.println(name + ", " + age);
+    }
+}
