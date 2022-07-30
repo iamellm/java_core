b545621295f9449a7fec3049e05589e9bb2b9f86
Evgeny Lim <lim.evny@gmail.com>
2022-07-31 04:29:17 +0900

    Shapes

---
+++ src/exercises/shapes/Color.java
@@ -0,0 +1,9 @@
+package exercises.shapes;
+
+public enum Color {
+    BLACK,
+    WHITE,
+    RED,
+    GREEN,
+    BLUE
+}
---
+++ src/exercises/shapes/Shape.java
@@ -0,0 +1,11 @@
+package exercises.shapes;
+
+public class Shape {
+    private final Color color;
+
+    public Shape(Color color) { this.color = color; }
+
+    public Color getColor() { return color; }
+
+    public double getArea() { return Double.NaN; }
+}
