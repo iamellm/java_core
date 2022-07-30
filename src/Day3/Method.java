7694e3e96d08e46477f2429e13a625d9ad37b1ee
Evgeny Lim <lim.evny@gmail.com>
2022-07-31 03:03:07 +0900

    Method sum of three numbers and its average

---
+++ src/Day3/Method.java
@@ -0,0 +1,24 @@
+package Day3;
+
+public class Method {
+
+    int sumOfThreeNumbers(int a, int b, int c) {
+        int rsl = a + b + c;
+        return rsl;
+    }
+
+    int averageSumOfThreeNumbers(int a1, int b1, int c1) {
+        int rsl = sumOfThreeNumbers(a1, b1, c1) /3;
+        return rsl;
+    }
+}
+
+class MethodTest{
+    public static void main(String[] args) {
+        Method method = new Method();
+        int sum = method.sumOfThreeNumbers(3, 8, 11);
+        System.out.println(sum);
+        System.out.println(method.sumOfThreeNumbers(10, 20, 30));
+        System.out.println(method.averageSumOfThreeNumbers(20, 40, 60));
+    }
+}
