15cb4e9322ba7e560b988ec737fd6ea940d4b4eb
Evgeny Lim <lim.evny@gmail.com>
2022-08-11 06:16:39 +0900

    carpet cost calculator

--- src/exercises/java_masterclass/oop/carpet_cost_calculator/Main.java
+++ src/exercises/java_masterclass/oop/carpet_cost_calculator/Main.java
@@ -2,8 +2,8 @@ package exercises.java_masterclass.oop.carpet_cost_calculator;
 
 public class Main {
     public static void main(String[] args) {
-        Carpet carpet = new Carpet(10);
-        Floor floor = new Floor(2.5, 4.0);
+        Carpet carpet = new Carpet(7.5);
+        Floor floor = new Floor(5.5, 4.0);
         Calculator calculator = new Calculator(floor, carpet);
         System.out.println("total cost = " + calculator.getTotalCost());
         carpet = new Carpet(5);
